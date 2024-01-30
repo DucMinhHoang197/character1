a679cce6ccc9e3c2ca5c45bbe91acb5abb182356
DucMinhHoang197 <ducminhhoang197@gmail.com>
2024-01-30 13:07:14 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,14 @@ const getSelections = (type, numOfElements) => {
 
 function App() {
   const [bodySelection, setBodySelection] = useState(0);
+  const [eyeSelection, setEyeSelection] = useState(0);
+  const [hairSelection, setHairSelection] = useState(0);
+  const [mouthSelection, setMouthSelection] = useState(0);
+  const [eyeBrowsSelection, setEyeBrowsSelection] = useState(0);
+  const [glassesSelection, setGlassesSelection] = useState(0);
+  const [clothesL1Selection, setClothesL1Selection] = useState(0);
+  const [clothesL2Selection, setClothesL2Selection] = useState(0);
+  const [clothesL3Selection, setClothesL3Selection] = useState(0);
   console.log(bodySelection);
   return (
     <div className="container">
@@ -32,23 +40,51 @@ function App() {
             heading="Body"
             data={getSelections("body", 30)}
           />
-          <List heading="Eye" data={getSelections("eyebrows", 30)} />
-          <List heading="Hair" data={getSelections("hair", 30)} />
-          <List heading="Mouth" data={getSelections("mouths", 30)} />
-          <List heading="Eyebrows" data={getSelections("eyebrows", 30)} />
           <List
+            selected={eyeSelection}
+            onChange={setEyeSelection}
+            heading="Eye"
+            data={getSelections("eyebrows", 30)}
+          />
+          <List
+            selected={hairSelection}
+            onChange={setHairSelection}
+            heading="Hair"
+            data={getSelections("hair", 30)}
+          />
+          <List
+            selected={mouthSelection}
+            onChange={setMouthSelection}
+            heading="Mouth"
+            data={getSelections("mouths", 30)}
+          />
+          <List
+            selected={eyeBrowsSelection}
+            onChange={setEyeBrowsSelection}
+            heading="Eyebrows"
+            data={getSelections("eyebrows", 30)}
+          />
+          <List
+            selected={glassesSelection}
+            onChange={setGlassesSelection}
             heading="Glasses"
             data={getSelections("accessories/glasses", 30)}
           />
           <List
+            selected={clothesL1Selection}
+            onChange={setClothesL1Selection}
             heading="Clothing(L1)"
             data={getSelections("clothes/layer_1", 30)}
           />
           <List
+            selected={clothesL2Selection}
+            onChange={setClothesL2Selection}
             heading="Clothing(L2)"
             data={getSelections("clothes/layer_2", 30)}
           />
           <List
+            selected={clothesL3Selection}
+            onChange={setClothesL3Selection}
             heading="Clothing(L3)"
             data={getSelections("clothes/layer_3", 30)}
           />
